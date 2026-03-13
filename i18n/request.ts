b2043cdba37a46c839a10568039342b2f26751ef
Kate Seo <violetds0826@gmail.com>
2026-03-13 19:59:27 -0400

    change message files location

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { routing } from './routing';
-import enMessages from '../../messages/en.json';
-import koMessages from '../../messages/ko.json';
+import enMessages from '../messages/en.json';
+import koMessages from '../messages/ko.json';
 
 const messagesMap: Record<string, any> = {
     en: enMessages,
