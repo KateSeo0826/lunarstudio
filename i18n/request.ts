5b9d277aa5918e362cb804fe1def9a83fe5ce5ac
Kate Seo <violetds0826@gmail.com>
2026-03-13 19:43:00 -0400

    fixed error

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,7 +1,6 @@
 import { getRequestConfig } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { routing } from './routing';
-
 import enMessages from '../../messages/en.json';
 import koMessages from '../../messages/ko.json';
 
--- next.config.mjs
+++ next.config.mjs
@@ -16,5 +16,6 @@ const nextConfig = {
     )
     ],
   },
+
 };
 export default withNextIntl(nextConfig);
