0a4c367c5c6ae0c8be4f1992f3fcf4b40b936692
Kate Seo <violetds0826@gmail.com>
2026-03-17 18:52:33 -0400

    changed nextcofing file

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig = {
-
   allowedDevOrigins: [
     'http://localhost:3000',
     'http://10.0.0.65:3000',
 
   ],
-  images: {
-    remotePatterns: [new URL('https://cdn.prod.website-files.com/68ee74b7102caefef6ce78ac/**'),
-    new URL('https://cdn.prod.website-files.com/692e410b4406ea2cf410f589/**'
-    )
-    ],
-  },
-
 };
 export default withNextIntl(nextConfig);
