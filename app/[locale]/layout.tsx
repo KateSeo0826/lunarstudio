3a33f62789065bf07cf159cddc2a31d813eca140
Kate Seo <violetds0826@gmail.com>
2026-03-19 23:50:04 -0400

    add google site verification

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -42,6 +42,12 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} className={`${pretendard.variable} ${dmSans.variable}`}>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="Yzr9M-zbO617FBym5Y-F0u8tm_X7BKs-YerMiIhzig0"
+        />
+      </head>
       <body className={pretendard.className}>
         <NextIntlClientProvider locale={locale}>
           <Navar />
