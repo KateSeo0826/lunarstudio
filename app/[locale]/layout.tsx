8ef2c090158005ed713337a122431c33bbc49bc8
Kate Seo <violetds0826@gmail.com>
2026-08-31 18:09:13 -0400

    Add Impact site verification meta tag

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -91,6 +91,10 @@ export default async function RootLayout({
           name="naver-site-verification"
           content="ad3ed6914ed37996162b48bc31cdbbb203aa4747"
         />
+        <meta
+          name="impact-site-verification"
+          value="c5c4d892-a0a7-4df7-bbea-cb3a1cc912e5"
+        />
         <link rel="alternate" href="https://lunar-studio.ca/ko" hrefLang="ko" />
         <link rel="alternate" href="https://lunar-studio.ca/en" hrefLang="en" />
         <Script
